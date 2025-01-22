8448acf45bbfef24c7af5c2be8ded2d363ad2a5b
Shushant Kumar Awasthi <sushantawasthi000@gmail.com>
2025-01-23 01:01:59 +0530

    programming

--- DAY6/assignment/script.js
+++ DAY6/assignment/script.js
@@ -1,5 +1,5 @@
-const ctaButton = document.getElementById('cta-button');
+const ctaButton = document.getElementById("cta-button");
 
-ctaButton.addEventListener('click', () => {
-  alert('Thank you for getting started with MyBrand!');
+ctaButton.addEventListener("click", () => {
+  alert("Thank you for getting started with Programming Pathshala!");
 });
